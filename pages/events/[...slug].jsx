809b51cf06516ae3bbb255390c3d2839417d2a0f
Andrey Osipov <mitol5549@gmail.com>
2023-08-22 18:32:37 +0200

    main Pages added

---
+++ pages/events/[...slug].jsx
@@ -0,0 +1,9 @@
+const FilteredEventsPage = () => {
+  return (
+    <>
+      <h1>Filtered Events</h1>
+    </>
+  );
+};
+
+export default FilteredEventsPage;
---
+++ pages/events/[eventId].jsx
@@ -0,0 +1,9 @@
+const EventDetailPage = () => {
+  return (
+    <>
+      <h1>Event Detail</h1>
+    </>
+  );
+};
+
+export default EventDetailPage;
