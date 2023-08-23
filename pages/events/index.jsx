02f11ca87bd211e49a94ce18144df77122056453
Andrey Osipov <mitol5549@gmail.com>
2023-08-23 16:34:49 +0200

    navigation to the Filtered Events Page added

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,14 +1,23 @@
+import { useRouter } from 'next/router';
+
 import { getAllEvents } from '../../dummy-data';
 
 import { EventList } from '../../components/events/EventList';
 import { EventsSearch } from '../../components/events/EventsSearch';
 
 const AllEventsPage = () => {
+  const router = useRouter();
   const events = getAllEvents();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  };
 
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList events={events} />
     </>
   );
