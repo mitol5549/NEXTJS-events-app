aa72f4adc728ff2e1159bbbae1b9a8030e10cbf0
Andrey Osipov <mitol5549@gmail.com>
2023-08-23 16:23:20 +0200

    Filtered Form added

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,12 +1,14 @@
 import { getAllEvents } from '../../dummy-data';
 
 import { EventList } from '../../components/events/EventList';
+import { EventsSearch } from '../../components/events/EventsSearch';
 
 const AllEventsPage = () => {
   const events = getAllEvents();
 
   return (
     <>
+      <EventsSearch />
       <EventList events={events} />
     </>
   );
