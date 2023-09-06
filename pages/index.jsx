25732a7c757a838ba1a58aa597b61f7f9c8f886d
Andrey Osipov <mitol5549@gmail.com>
2023-09-06 12:26:56 +0200

    Head content added

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,16 @@
-import { EventList } from '../components/events/EventList';
+import Head from 'next/head';
 
 import { getFeaturedEvents } from '../helpers/api-util';
+
+import { EventList } from '../components/events/EventList';
 
 export default function HomePage(props) {
   return (
     <>
+      <Head>
+        <title>Next Events</title>
+        <meta name="description" content="Find a lot of great events that allow you to evolve..." />
+      </Head>
       <EventList events={props.featuredEvents} />
     </>
   );
