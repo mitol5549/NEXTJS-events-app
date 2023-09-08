c2daad138e854e4c77816d83072d3c7f4435f13b
Andrey Osipov <mitol5549@gmail.com>
2023-09-08 16:51:13 +0200

    NextUI added, Navbar changed

--- components/layout/MainHeader.jsx
+++ components/layout/MainHeader.jsx
@@ -1,20 +1,20 @@
-import Link from 'next/link';
-
-import classes from './MainHeader.module.css';
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from '@nextui-org/react';
 
 export const MainHeader = () => {
   return (
-    <header className={classes.header}>
-      <div className={classes.logo}>
-        <Link href="/">Next Events</Link>
-      </div>
-      <nav className={classes.navigation}>
-        <ul>
-          <li>
-            <Link href="/events">Browse All Events</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <Navbar isBordered>
+      <NavbarBrand>
+        <Link href="/" className="font-bold text-inherit">
+          NEXT EVENTS
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Button as={Link} color="primary" href="/events" variant="flat">
+            Browse All Events
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
   );
 };
--- pages/_document.js
+++ pages/_document.js
@@ -3,10 +3,10 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="en" className="light">
         <Head />
         <body>
-          <div id='overlays' />
+          <div id="overlays" />
           <Main />
           <NextScript />
         </body>
