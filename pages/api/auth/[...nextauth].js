f114ed33dcdf21782eda351647bcfad0009e0ddc
Andrey Osipov <mitol5549@gmail.com>
2023-09-26 16:33:51 +0200

    NextAuth added

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,36 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+import { connectDatabase, findOne } from '../../../helpers/db-util';
+import { verifyPassword } from '../../../helpers/auth';
+
+export default NextAuth({
+  session: {
+    jwt: true,
+  },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        const client = await connectDatabase();
+
+        const user = await findOne(client, 'users', { email: credentials.email });
+
+        if (!user) {
+          client.close();
+          throw new Error('No user found!');
+        }
+
+        const isValid = await verifyPassword(credentials.password, user.password);
+
+        if (!isValid) {
+          client.close();
+          throw new Error('Could not log you in!');
+        }
+
+        client.close();
+
+        return { email: user.email };
+      },
+    }),
+  ],
+});
--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,4 +1,4 @@
-import UserProfile from '../components/profile/UserProfile';
+import { UserProfile } from '../components/profile/UserProfile';
 
 export default function ProfilePage() {
   return <UserProfile />;
